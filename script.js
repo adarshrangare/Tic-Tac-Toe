f98fd7bef1493d387751a0802887a96ce0bb91a8
adarshrangare <akrangare@gmail.com>
2023-10-04 22:55:53 +0530

    modified

--- script.js
+++ script.js
@@ -1,27 +1,69 @@
 console.log("Script Started");
 
-const grid = document.querySelector(".game");
+let grid = document.querySelectorAll("input");
+
+grid = Array.from(grid);
 
 console.log(grid);
-
+let clickCount = 0;
 let firstTurn = true;
 
+const winCombination = [
+  [11, 12, 13],
+  [11, 21, 31],
+  [11, 22, 33],
+  [21, 22, 23],
+  [31, 32, 33],
+  [21, 22, 23],
+  [31, 32, 33],
+  [11, 21, 31]
+];
 
-grid.addEventListener("click",(e)=>{
-    console.log(e);
+grid.map((gridEle) => {
+
+  gridEle.addEventListener("click", (e) => {
+
+    // console.log(e);
 
     console.log(e.target);
 
-  if(firstTurn){
-    e.target.value = 'X';
-    firstTurn=false;
+
+    if (firstTurn) {
+      e.target.value = 'X';
+      firstTurn = false;
+
+    } else {
+      e.target.value = 'O';
+      firstTurn = true;
+    }
+    clickCount++;
+
+    e.target.setAttribute("disabled", "");
+    console.log(clickCount);
     
-  }else{
-    e.target.value = 'O';
-    firstTurn=true;
-  }
-  e.target.setAttribute("disabled","");
+
+  });
+})
+
+
+
+// Check Winner
+
+  
+
+
+
+
+// RESET BUTTON
+
+const resetbtn = document.querySelector("#reset");
+console.log(resetbtn);
+
+resetbtn.addEventListener("click", (e) => {
+  console.log("clicked");
+  grid.map((gridEle) => {
+    gridEle.removeAttribute("disabled");
+    gridEle.value = ""
+  })
 
 });
-
-
